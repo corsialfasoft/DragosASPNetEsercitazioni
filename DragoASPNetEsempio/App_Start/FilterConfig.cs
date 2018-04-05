394edbb153ad13e5da07267212ba6298f79111b8
TandyD <brinzila30@gmail.com>
2018-04-05 15:11:37 +0200

    Aggiungere i file di progetto.

---
+++ DragoASPNetEsempio/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DragoASPNetEsempio {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DragoASPNetEsempio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DragoASPNetEsempio.MvcApplication" Language="C#" %>
